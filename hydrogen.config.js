6b33eae6bd2cfaf18083e5fa6090611087b82236
Andre Almeida <andrealmeida@MacBook-Air.local>
2026-01-29 18:02:34 -0300

    fix: Add proper Hydrogen dependencies and config

---
+++ hydrogen.config.js
@@ -0,0 +1,11 @@
+import {defineConfig} from '@shopify/hydrogen/config';
+
+export default defineConfig({
+  shopify: {
+    defaultCountryCode: 'BR',
+    defaultLanguageCode: 'PT',
+    storeDomain: process.env.PUBLIC_STORE_DOMAIN || '',
+    storefrontToken: process.env.PUBLIC_STOREFRONT_API_TOKEN || '',
+    storefrontApiVersion: '2024-10',
+  },
+});
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,27 @@
-import { defineConfig } from 'vite';
-import { vitePlugin as remix } from '@remix-run/dev';
-import { hydrogen } from '@shopify/hydrogen/vite';
+import {defineConfig} from 'vite';
+import {hydrogen} from '@shopify/hydrogen/vite';
+import {oxygen} from '@shopify/mini-oxygen/vite';
+import {vitePlugin as remix} from '@remix-run/dev';
 
 export default defineConfig({
-  plugins: [hydrogen(), remix()],
+  plugins: [
+    hydrogen(),
+    oxygen(),
+    remix({
+      presets: [hydrogen.preset()],
+      future: {
+        v3_fetcherPersist: true,
+        v3_relativeSplatPath: true,
+        v3_throwAbortReason: true,
+      },
+    }),
+  ],
+  build: {
+    assetsInlineLimit: 0,
+  },
+  ssr: {
+    optimizeDeps: {
+      include: [],
+    },
+  },
 });
