8c817159d972700adbb247ca27ce84aad1d28535
Andre Almeida <andrealmeida@MacBook-Air.local>
2026-01-30 01:53:58 -0300

    replace polyfills with vite-plugin-node-polyfills

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import {defineConfig} from 'vite';
 import {hydrogen} from '@shopify/hydrogen/vite';
 import {vitePlugin as remix} from '@remix-run/dev';
 import tsconfigPaths from 'vite-tsconfig-paths';
+import {nodePolyfills} from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
   plugins: [
@@ -15,30 +16,47 @@ export default defineConfig({
       },
     }),
     tsconfigPaths(),
+    nodePolyfills({
+      include: [
+        'buffer',
+        'process',
+        'util',
+        'stream',
+        'assert',
+        'events',
+        'crypto',
+        'http',
+        'https',
+        'os',
+        'path',
+        'querystring',
+        'url',
+        'zlib',
+        'fs',
+        'net',
+        'tls',
+      ],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      protocolImports: true,
+    }),
   ],
   ssr: {
-    optimizeDeps: {
-      include: [],
-    },
     noExternal: true,
     resolve: {
       conditions: ['workerd', 'worker', 'browser'],
       externalConditions: ['workerd', 'worker'],
     },
   },
-  resolve: {
-    alias: {
-      'stream': 'stream-browserify',
-      'buffer': 'buffer/',
-      'assert': 'assert/',
-      'util': 'util/',
-      'process': 'process/browser',
-    },
-  },
-  define: {
-    'process.env.NODE_DEBUG': 'false',
-  },
   build: {
     assetsInlineLimit: 0,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined,
+      },
+    },
   },
 });
