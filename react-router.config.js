c05f81fbe4eaa6e9d8e1ba9d668a9e449ccbdf57
Andre Almeida <andrealmeida@Andres-MacBook-Air.local>
2026-04-07 16:00:02 -0300

    fix: remove hydrogenPreset to disable auto middleware in Hydrogen 2025.7

--- react-router.config.js
+++ react-router.config.js
@@ -1,8 +1,3 @@
-import {hydrogenPreset} from '@shopify/hydrogen/react-router-preset';
-
-/**
- * React Router 7.9.x Configuration for Hydrogen
- */
 export default {
-  presets: [hydrogenPreset()],
+  ssr: true,
 };
